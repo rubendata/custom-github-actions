e4b8c8b82c990dfc2debf171cad4711941408cc2
rubendata <info@rubensimon.de>
2023-09-03 19:24:40 +0200

    test

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const core = require('@actions/core');
-const moment = require('moment');
+console.log("hallo")
+// const core = require('@actions/core');
+// const moment = require('moment');
 
-try {
-  const name = core.getInput('name');
-  console.log(`Hello, ${name}. We appreciate your business!`);
-  const timestamp = moment().format();
-  console.log(`Greeting issued at: ${timestamp}`);
-  core.setOutput("timestamp", timestamp);
-} catch (error) {
-  core.setFailed(error.message);
-}
+// try {
+//   const name = core.getInput('name');
+//   console.log(`Hello, ${name}. We appreciate your business!`);
+//   const timestamp = moment().format();
+//   console.log(`Greeting issued at: ${timestamp}`);
+//   core.setOutput("timestamp", timestamp);
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
