3469b14f20b85264f9cf058acdebe81f9b16eb7c
rubendata <info@rubensimon.de>
2023-09-03 19:31:53 +0200

    test package

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 console.log("hallo")
-// const core = require('@actions/core');
-// const moment = require('moment');
+const core = require('@actions/core');
+const moment = require('moment');
 
-// try {
-//   const name = core.getInput('name');
-//   console.log(`Hello, ${name}. We appreciate your business!`);
-//   const timestamp = moment().format();
-//   console.log(`Greeting issued at: ${timestamp}`);
-//   core.setOutput("timestamp", timestamp);
-// } catch (error) {
-//   core.setFailed(error.message);
-// }
+try {
+  const name = core.getInput('name');
+  console.log(`Hello, ${name}. We appreciate your business!`);
+  const timestamp = moment().format();
+  console.log(`Greeting issued at: ${timestamp}`);
+  core.setOutput("timestamp", timestamp);
+} catch (error) {
+  core.setFailed(error.message);
+}
